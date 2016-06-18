1a8a2b842d9c90b281229e2abc80e1e19327acf4
Egor Rychkov <biospore@hackerdom.ru>
2016-06-18 13:19:23 +0500

    Working 'Add Note' functional added

--- app/src/main/java/biospore/yandex_test/AddNoteActivity.java
+++ app/src/main/java/biospore/yandex_test/AddNoteActivity.java
@@ -1,8 +1,10 @@
 package biospore.yandex_test;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class AddNoteActivity extends AppCompatActivity {
 
@@ -13,5 +15,19 @@ public class AddNoteActivity extends AppCompatActivity {
     }
 
     public void addNote(View view) {
+        NoteDatabaseHelper db = new NoteDatabaseHelper(this);
+        EditText title = (EditText) findViewById(R.id.title_field);
+        EditText text =  (EditText) findViewById(R.id.text_field);
+        /*Working with sql very slow*/
+        if (text != null && title != null) {
+            if (title.getText().toString() != "" && text.getText().toString() != "")
+                /*field not unique - don't need additional checking*/
+                db.addNote(new Note(
+                        title.getText().toString(),
+                        text.getText().toString()
+                ));
+            //else {/*Warning that title is empty or text is empty*/}
+        }
+        finish();
     }
 }
--- app/src/main/java/biospore/yandex_test/MainActivity.java
+++ app/src/main/java/biospore/yandex_test/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -18,8 +19,8 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private ListView listView;
-//    private Button button;
     public static ArrayList<String> titles = new ArrayList<String>();
+    NoteDatabaseHelper db;
 
 
     @Override
@@ -27,46 +28,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        NoteDatabaseHelper db = new NoteDatabaseHelper(this);
-        List<Note> notes = db.getAllNotes();
-
+        db = new NoteDatabaseHelper(this);
         listView = (ListView) findViewById(R.id.list_view);
-//        button = (Button) findViewById(R.id.button);
+        createAdapter();
+        refreshContent();
+    }
 
-
-
+    private void createAdapter()
+    {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 this,
                 android.R.layout.simple_list_item_1,
                 titles
                 );
+        listView.setAdapter(adapter);
+    }
+
+    private void refreshContent()
+    {
+        titles.clear();
+        ArrayAdapter adapter = (ArrayAdapter) listView.getAdapter();
+        ((ArrayAdapter)listView.getAdapter()).notifyDataSetChanged();
+
+        List<Note> notes = db.getAllNotes();
 
         for (Note n : notes)
         {
             adapter.add(n.getTitle() + String.valueOf(n.getId()));
         }
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(
-                new AdapterView.OnItemClickListener() {
-
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        Log.d("long id", String.valueOf(id));
-                    }
-                });
-//        listView.setOnItemClickListener(
-//                new AdapterView.OnItemClickListener() {
-//                    @Override
-//                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                        Toast.makeText(getApplicationContext(), ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
-//                    }
-//                }
-//        );
     }
 
     public void addNote(View view)
     {
         Intent intent = new Intent(this, AddNoteActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        refreshContent();
     }
 }

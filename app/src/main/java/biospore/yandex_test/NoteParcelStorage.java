9684a693300f5cd705b742d7685da35bd1f583cd
Egor Rychkov <hsxrjd@yandex-team.ru>
2016-07-12 16:38:15 +0500

    Добавлен класс NoteParcelStorage

---
+++ app/src/main/java/biospore/yandex_test/NoteParcelStorage.java
@@ -0,0 +1,53 @@
+package biospore.yandex_test;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.ArrayList;
+
+/**
+ * Created by hsxrjd on 12.07.16.
+ */
+public class NoteParcelStorage implements Parcelable {
+    public ArrayList<Note> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(ArrayList<Note> notes) {
+        this.notes = notes;
+    }
+
+    private ArrayList<Note> notes;
+    public NoteParcelStorage(ArrayList<Note> in)
+    {
+        notes = in;
+    }
+
+    protected NoteParcelStorage(Parcel in)
+    {
+        notes = in.readArrayList(Note.class.getClassLoader());
+    }
+
+    public static final Creator<NoteParcelStorage> CREATOR = new Creator<NoteParcelStorage>() {
+        @Override
+        public NoteParcelStorage createFromParcel(Parcel in) {
+            return new NoteParcelStorage(in);
+        }
+
+        @Override
+        public NoteParcelStorage[] newArray(int size) {
+            return new NoteParcelStorage[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags)
+    {
+        dest.writeList(notes);
+    }
+}
